7ec0bc8c3f9447cff07e4d409c9ec1b59ff89e26
Sergio Fuenmayor <sfuenmayor@imenta.net>
2020-02-17 11:41:59 -0300

    React Animation Components

--- src/components/MenuDetail.js
+++ src/components/MenuDetail.js
@@ -6,6 +6,8 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+
 
     function RenderDish(props){
       if (props.isLoading) {
@@ -28,13 +30,19 @@ import { baseUrl } from '../shared/baseUrl';
     }
     else if (props.dish != null) 
       return(
-        <Card>
-          <CardImg top src={baseUrl + props.dish.image} alt={props.dish.name} />    
-          <CardBody>
-          <CardTitle>{props.dish.name}</CardTitle>
-          <CardText>{props.dish.description}</CardText>
-          </CardBody>
-        </Card>
+        <FadeTransform
+        in
+        transformProps={{
+            exitTransform: 'scale(0.5) translateY(-50%)'
+        }}>
+          <Card>
+            <CardImg top src={baseUrl + props.dish.image} alt={props.dish.name} />    
+            <CardBody>
+            <CardTitle>{props.dish.name}</CardTitle>
+            <CardText>{props.dish.description}</CardText>
+            </CardBody>
+          </Card>
+        </FadeTransform>  
       );
     }    
 
@@ -42,7 +50,14 @@ import { baseUrl } from '../shared/baseUrl';
 
       const commentList = comments.map((comment) => {
         return (
-            <li key={comment.id}>{comment.id} -- {comment.author} -- {comment.comment}</li>
+          <Stagger in>
+            <Fade in>
+            <li key={comment.id}>
+              <p>{comment.comment}</p>
+              <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+            </li>
+            </Fade>
+          </Stagger>
         );
         });
 
